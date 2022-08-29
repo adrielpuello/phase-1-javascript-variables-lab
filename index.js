878caa50a099ce8af8f0847b914310decc5f14b7
Adriel Puello <adrielpuello@gmail.com>
2022-08-29 10:54:42 -0400

    commit tree 5ebc5df6c56f5e987be5c2953087435a5cc7804a

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solution in this file!
+// index.js
+//companyName
+//1) is set as Scuber
+///2) is defined as a const
+//mostProfitableNeighborhood
+//3) is declared as equal to Chelsea
+//4) is defined using let
+//companyCeo
+//5) is declared as equal to Susan Smith
+//6) is defined using let
+const companyName = 'Scuber'
+let mostProfitableNeighborhood = "Chelsea"
+let companyCeo = 'Susan Smith'
